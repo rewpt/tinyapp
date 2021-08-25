797cae120fc8d858c8b153f25ec68db167b3207c
rewpt <zpermack@gmail.com>
2021-08-25 02:26:02 +0000

    add get method for urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -22,6 +28,10 @@ app.get("/urls.json", (req, res) => {
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls", (req, res) => {
